d67e0e7d72222a4a6398347478d0e14dc268ba32
Somnathumapathi <somnathumapathi7@gmail.com>
2024-02-13 00:56:30 +0530

    Sign up and login

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ mongoose.connect(DB).then(()=>{
     console.log(`Error: ${e}`)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Connected at port ${PORT}`)
 })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const authRouter = express.Router()
 
 authRouter.post("/api/signup", async (req, res) => {
@@ -10,14 +11,33 @@ try{
  if(currentUser) {
     return res.status(400).json({msg:'Email already in use'})
  }
+ const hashedPass = await bcrypt.hash(password, 7)
  let user = new User ({
-    email, name, password
+    email, name, password: hashedPass,
  })
  user = await user.save()
  res.json(user)
 } catch (e) {
     res.status(500).json({error: e.message})
 }
+})
+
+authRouter.post('/api/login', async (req, res) => {
+   try{
+const {email,password} = req.body
+const user = await User.findOne({email})
+if(!user) {
+   return res.status(400).json({msg: 'User does not exist'})
+}
+ const isMatch = await bcrypt.compare(password, user.password)
+ if(!isMatch) {
+   return res.status(400).json({msg: 'Wrong password'})
+ }
+const token = jwt.sign({id: user._id}, 'passwordKey')
+res.json({token, ...user._doc})
+   } catch (e) {
+      res.status(500).json({error: e.message})
+   }
 })
 
 module.exports = authRouter
